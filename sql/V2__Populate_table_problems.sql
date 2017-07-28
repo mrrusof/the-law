8717f57b6a1294f33e486fbe4ece0ad3b4c53e25
Ruslan Ledesma Garza <ruslanledesmagarza@gmail.com>
2017-07-28 00:17:03 -0500

    Add another example problem.

--- sql/V2__Populate_table_problems.sql
+++ sql/V2__Populate_table_problems.sql
@@ -5,4 +5,11 @@ insert into problems (id, title, summary, statement, solution_url) values
 'Print all strings consisting of n balanced parentheses.',
 'Problem statement "All Balanced Parentheses Strings".',
 'http://ruslanledesma.com/2017/05/16/all-balanced-parentheses-strings.html'
+),
+
+(2,
+'Merge by Buffer',
+'Write a function that merges two sorted integer arrays using no other buffer than the one you are given.',
+'Problem statement "Merge by Buffer".',
+'http://ruslanledesma.com/2017/04/13/merge-by-buffer.html'
 );
