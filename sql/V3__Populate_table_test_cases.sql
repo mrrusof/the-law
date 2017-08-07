8369f7fe4e2c71027bd29e329118e2f12bddfdd9
Ruslan Ledesma Garza <ruslanledesmagarza@gmail.com>
2017-08-07 00:18:48 -0500

    Write problem statements in markdown, add test case.

--- sql/V3__Populate_table_test_cases.sql
+++ sql/V3__Populate_table_test_cases.sql
@@ -43,3 +43,15 @@ insert into test_cases (problem_id, input, output) values
 (()())
 ((()))
 ');
+
+
+insert into test_cases (problem_id, input, output) values
+
+(
+'merge-by-buffer',
+'6
+1 5 10 20 30 40 -1 -1 -1 -1
+3 8 25
+','9
+1 3 5 8 10 20 25 30 40 40
+');
